d17ceb21208da0effcb26c60527411b3ddc6998e
jchen42703 <jchen42703@gmail.com>
2020-09-06 18:28:41 -0400

    Added the prediction pipeline
    Note that the mouse moving should be done instead a promise

--- js/index.js
+++ js/index.js
@@ -9,9 +9,26 @@ app.get("/", (req, res) => {
   // res.send(model);
   res.sendFile(path.join(__dirname, relativeModelPath));
   console.log(`dir: ${path.join(__dirname, relativeModelPath)}`);
-  console.log(predict.loadModel(relativeModelPath));
-  let model = predict.loadModel(relativeModelPath);
-  console.log(model);
+  const pred_promise = predict.loadAndPredict(
+    relativeModelPath,
+    [1, 1],
+    [82, 33]
+  );
+  pred_promise.then((pred_promise) => {
+    var pred = pred_promise;
+    console.log(`prediction: ${pred}`);
+    // move mouse....
+  });
+
+  /**
+   * predict.loadModel -> predict.getModelPred pipeline
+   * awkward, but more readable
+   */
+  // let model = predict.loadModel(relativeModelPath);
+  // const pred = model.then((model) => {
+  //   predict.getModelPred(model, [1, 1], [82, 33]);
+  // });
+  // console.log(`Final prediction: ${pred}`);
 });
 
 const PORT = process.env.PORT || 3000;
