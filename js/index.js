da9dbde2fb207adac04d6a5eb1b3d19613ca8257
jchen42703 <jchen42703@gmail.com>
2020-09-10 02:02:21 -0400

    Updated to handle POST requests to "/"
    POST the start/destination coordinates

--- js/index.js
+++ js/index.js
@@ -1,19 +1,21 @@
 const predict = require("./src/predict.js");
 const mouse = require("./src/mouse.js");
 const path = require("path");
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 
 let relativeModelPath = "src/model/tfjs_model/model.json";
 
-app.get("/", (req, res) => {
-  // res.send(model);
-  res.sendFile(path.join(__dirname, relativeModelPath));
+app.use(bodyParser.json());
+
+app.post("/", (req, res) => {
+  console.log(req.body);
   console.log(`dir: ${path.join(__dirname, relativeModelPath)}`);
   const pred_promise = predict.loadAndPredict(
     relativeModelPath,
-    [1, 1],
-    [82, 33]
+    req.body.start,
+    req.body.destination
   );
   pred_promise.then((pred_promise) => {
     var pred = pred_promise;
@@ -21,18 +23,13 @@ app.get("/", (req, res) => {
     // move mouse....
     mouse.moveMousePath(pred);
   });
+  res.end("Movement done!");
+});
 
-  /**
-   * predict.loadModel -> predict.getModelPred pipeline
-   * awkward, but more readable
-   */
-  // let model = predict.loadModel(relativeModelPath);
-  // const pred = model.then((model) => {
-  //   predict.getModelPred(model, [1, 1], [82, 33]);
-  // });
-  // console.log(`Final prediction: ${pred}`);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, relativeModelPath));
+  res.end("File sent!");
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
-// app.listen(PORT, () => getModel());
