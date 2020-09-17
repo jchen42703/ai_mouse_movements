5ea47eb8d7865aee5f0d708f2da0f1d0c238d8bc
jchen42703 <jchen42703@gmail.com>
2020-09-17 12:47:23 -0400

    Fixed the LSTM model to accomodate for destination -> generate path (changed fn name to lstm_ann())

--- python/pymouse/models/lstm.py
+++ python/pymouse/models/lstm.py
@@ -5,22 +5,19 @@ from tensorflow.keras.optimizers import Adam
 
 target_path_count = 100
 
-target_path_count = 100
-
-def generate_model():
+def lstm_ann():
     model = Sequential()
-    model.add(LSTM(128, return_sequences=False, input_shape=(3, 100)))
-    #model.add(LSTM(32, return_sequences=True))
-    #model.add(LSTM(32))
-    model.add(Dense(100, activation='relu'))
+    model.add(Dense(500, activation='relu', input_dim=2))
+    model.add(Dense(400, activation='relu'))
     model.add(Dense(target_path_count * 3, activation='linear'))
     model.add(Reshape((target_path_count, 3)))
-    # model.summary()
+    model.add(LSTM(300, return_sequences=False, input_shape=(100, 3)))
+    model.add(Reshape((target_path_count, 3)))
     opt = Adam(lr=1e-3)
     model.compile(loss='mean_squared_error', optimizer=opt,
                   metrics=['mean_squared_error'])
     return model
 
 if __name__ == '__main__':
-    model = generate_model()
+    model = lstm_ann()
     model.summary()
