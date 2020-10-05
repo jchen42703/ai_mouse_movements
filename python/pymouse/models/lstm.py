b2ff4893e428dda37a5c50c7eac8e34210adb8e6
jchen42703 <jchen42703@gmail.com>
2020-10-04 23:07:42 -0400

    Added larger models (2.5 mil and 3.9 mil params)

--- python/pymouse/models/lstm.py
+++ python/pymouse/models/lstm.py
@@ -1,5 +1,5 @@
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, Reshape, LSTM
+from tensorflow.keras import Sequential, Model
+from tensorflow.keras.layers import Input, Dense, Reshape, LSTM
 from tensorflow.keras.optimizers import Adam
 
 
@@ -36,7 +36,76 @@ def lstm_ann2(clipvalue=1):
                   metrics=['mean_squared_error'])
     return model
 
+def lstm_ann_multiple(clipvalue=1):
+    """1.2 million (2 outputs: the path and the destination)
+    """
+    inputs = Input(shape=(2,))
+    dense1 = Dense(1000, activation='relu')(inputs)
+    dense2 = Dense(400, activation='relu')(dense1)
+    dense3 = Dense(target_path_count * 3, activation='linear')(dense2)
+    reshaped_path1 = Reshape((target_path_count, 3))(dense3)
+    lstm1 = LSTM(300, return_sequences=False,
+                 input_shape=(100, 3))(reshaped_path1)
+    reshaped_path2 = Reshape((target_path_count, 3))(lstm1)
+    lstm2 = LSTM(300, return_sequences=False,
+                 input_shape=(100, 3))(reshaped_path2)
+    path_out = Reshape((target_path_count, 3), name='path_out')(lstm2)
+    dest_out = path_out[-1]
+    model = Model(inputs=inputs, outputs=[path_out, dest_out])
+    opt = Adam(lr=1e-3, clipvalue=clipvalue)
+    model.compile(loss={'path_out': 'mse', 'dest_out': 'mse'}, optimizer=opt)
+    return model
+
+
+def lstm_ann_2mill(clipvalue=1, lr=1e-3):
+    """2.5 million params.
+
+    Assumes that the data is ranged from [0, 1] (normally [-1, 1])
+    """
+    inputs = Input(shape=(2,))
+    dense1 = Dense(800, activation='relu')(inputs)
+    dense2 = Dense(400, activation='relu')(dense1)
+    dense3 = Dense(target_path_count * 3, activation='linear')(dense2)
+    reshaped_path1 = Reshape((target_path_count, 3))(dense3)
+
+    lstm1 = LSTM(500, return_sequences=True,
+                 input_shape=(100, 3))(reshaped_path1)
+    lstm2 = LSTM(300, return_sequences=False, input_shape=(100, 3))(lstm1)
+    dense4 = Dense(target_path_count * 3, activation='linear')(lstm2)
+    path_out = Reshape((target_path_count, 3), name='path_out')(dense4)
+    model = Model(inputs=inputs, outputs=path_out)
+
+    opt = Adam(lr=lr, clipvalue=clipvalue)
+    model.compile(loss='mse', optimizer=opt)
+    return model
+
+
+def lstm_ann_4mill(clipvalue=1, lr=1e-3):
+    """4 million params.
+
+    Assumes that the data is ranged from [0, 1] (normally [-1, 1])
+    """
+    inputs = Input(shape=(2,))
+    dense1 = Dense(800, activation='relu')(inputs)
+    dense2 = Dense(400, activation='relu')(dense1)
+    dense3 = Dense(target_path_count * 3, activation='linear')(dense2)
+    reshaped_path1 = Reshape((target_path_count, 3))(dense3)
+
+    lstm1 = LSTM(500, return_sequences=True,
+                 input_shape=(100, 3))(reshaped_path1)
+    lstm2 = LSTM(300, return_sequences=True, input_shape=(100, 3))(lstm1)
+    lstm3 = LSTM(300, return_sequences=True, input_shape=(100, 3))(lstm2)
+
+    lstm4 = LSTM(300, return_sequences=False, input_shape=(100, 3))(lstm3)
+    dense4 = Dense(target_path_count * 3, activation='linear')(lstm4)
+    path_out = Reshape((target_path_count, 3), name='path_out')(dense4)
+    model = Model(inputs=inputs, outputs=path_out)
+
+    opt = Adam(lr=lr, clipvalue=clipvalue)
+    model.compile(loss='mse', optimizer=opt)
+    return model
+
 
 if __name__ == '__main__':
-    model = lstm_ann()
+    model = lstm_ann_2mill()
     model.summary()
