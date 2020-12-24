b47ba7c953d506f417043454ed1564e7820e677e
jchen42703 <jchen42703@gmail.com>
2020-12-23 23:13:15 -0500

    Commented out console.log

--- js/index.js
+++ js/index.js
@@ -10,8 +10,8 @@ let relativeModelPath = "src/model/tfjs_model/model.json";
 app.use(bodyParser.json());
 
 app.post("/", (req, res) => {
-  console.log(req.body);
-  console.log(`dir: ${path.join(__dirname, relativeModelPath)}`);
+  // console.log(req.body);
+  // console.log(`dir: ${path.join(__dirname, relativeModelPath)}`);
   let api = new predAPI.predictAPI(
     relativeModelPath,
     req.body.start,
@@ -20,7 +20,7 @@ app.post("/", (req, res) => {
   const pred_promise = api.predict();
   pred_promise.then((pred_promise) => {
     var pred = pred_promise;
-    console.log(`prediction: ${pred}, shape: ${pred.shape}`);
+    // console.log(`prediction: ${pred}, shape: ${pred.shape}`);
     // move mouse....
     mouse.moveMousePath(pred);
   });
