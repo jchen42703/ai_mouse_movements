904827545262886863836c96ea69e185133bd243
Joseph <jchen42703@gmail.com>
2020-12-29 17:39:03 -0500

    Added coords outlier removal for v2

--- python/pymousegan/io/preprocessor_v2.py
+++ python/pymousegan/io/preprocessor_v2.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from .preprocessor import mode
+
 
 def json_to_numpy(data_list):
     """Converts the original data.json to a numpy array
@@ -48,7 +50,7 @@ def minmax_normalize(arr, norm_range=[-1, 1], minmax=None):
             min (float): minimum of the dataset
             max (float): maximum of the dataset
         norm_range: list of 2 integers specifying normalizing range
-            based on https://stats.stackexchange.com/questions/178626/how-to-normalize-data-between-1-and-1
+
     Returns:
         Normalized array with outliers clipped in the specified range
     """
@@ -70,7 +72,6 @@ def minmax_unnormalize(norm_arr, minmax, norm_range=[-1, 1]):
             min (float): minimum of the original test set (for prediction)
             max (float): maximum of the original test set (for prediction)
         norm_range: list of 2 integers specifying normalizing range
-            based on https://stats.stackexchange.com/questions/178626/how-to-normalize-data-between-1-and-1   
 
     Returns:
         the rescaled array
@@ -93,27 +94,71 @@ def remove_bad_indices(paths, bad_indices):
     return paths[all_idx]
 
 
-def remove_outliers(coords_dt, dt_thresh=1000, elapsed_thresh=2000):
+def get_oob_paths_idx(paths, value_thresh=1):
+    """Get indices for the paths that have values > 1.
+    """
+    bad_indices = np.unique(np.where(paths > value_thresh)[0])
+    print(f'Number of Paths with Coords > 1: {len(bad_indices)}')
+    return bad_indices
+
+
+def get_excessive_loops_idx(paths, num_loops=30):
+    """Get indices for the paths that have excessive loops.
+    Problem: Also removes a lot of paths that go straight up, but it's
+    kinda okay because a lot of those paths are janky af.
+    """
+    bad_indices = []
+    for i, arr in enumerate(paths):
+        if mode(arr[:, 0])[1] >= num_loops:
+            bad_indices.append(i)
+    print(f'Number of Excessive Loops: {len(bad_indices)}')
+    return bad_indices
+
+
+def get_nan_idx(self, paths):
+    """When the destination has 0 in either X, Y
+    """
+    bad_indices = np.unique(np.where(np.isnan(paths))[0])
+    print(f'Number of Paths with NaN: {len(bad_indices)}')
+    return bad_indices
+
+
+def get_bad_dt(dt, dt_thresh=1000, elapsed_thresh=2000):
+    dt_bad = np.unique(np.where(dt > dt_thresh)[0]).tolist()
+    elapsed = np.sum(dt, axis=1)
+    elapsed_bad = np.unique(np.where(elapsed > elapsed_thresh)[0]).tolist()
+    print(f'{len(dt_bad)} paths that have a lag > dt_thresh.')
+    print(f'{len(elapsed_bad)} paths that exceed elapsed_thresh.')
+    return dt_bad + elapsed_bad
+
+
+def remove_outliers(coords_dt, dt_thresh=1000, elapsed_thresh=2000,
+                    value_thresh=1.05, num_loops=30):
     """Removes outliers from coords_dt array.
 
     Currently, only removes slow paths (when a path has a dt > dt_thresh OR
     when the total elapsed time is > elapsed_thresh).
     """
-    # looking only at the dt rn
-    dt_bad = np.unique(np.where(coords_dt[:, :, -1] > dt_thresh)[0]).tolist()
-    elapsed = np.sum(coords_dt[:, :, -1], axis=1)
-    elapsed_bad = np.unique(np.where(elapsed > elapsed_thresh)[0]).tolist()
-    print(f'{len(dt_bad)} paths that have a lag > dt_thresh.')
-    print(f'{len(elapsed_bad)} paths that exceed elapsed_thresh.')
-    bad_path_idx = np.unique(elapsed_bad + dt_bad)
+    # looking only at the dt
+    bad_dt = get_bad_dt(coords_dt[:, :, -1], dt_thresh=dt_thresh,
+                        elapsed_thresh=elapsed_thresh)
+
+    # looking at the coords
+    oob = get_oob_paths_idx(coords_dt[:, :, :-1], value_thresh=value_thresh)
+    excessive = get_excessive_loops_idx(coords_dt[:, :, :-1],
+                                        num_loops=num_loops)
+    # removing the bad paths
+    bad_path_idx = np.unique(bad_dt + oob + excessive)
     print(f'{len(bad_path_idx)} total number of outliers.')
     return remove_bad_indices(coords_dt, bad_path_idx)
 
 
-def preprocess(data_list, dt_thresh=1000, elapsed_thresh=1500):
+def convert_and_preprocess(data_list, dt_thresh=1000, elapsed_thresh=1500,
+                           value_thresh=1.05, num_loops=30):
     coords_dt = json_to_numpy(data_list)
     coords_dt[:, :, -1] = t_to_dt(coords_dt[:, :, -1])
-    coords_dt = remove_outliers(coords_dt, dt_thresh, elapsed_thresh)
+    coords_dt = remove_outliers(coords_dt, dt_thresh, elapsed_thresh,
+                                value_thresh=value_thresh, num_loops=num_loops)
     print('Before minmax normalization;' +
           f'dt min: {coords_dt[:, :, -1].min()}' +
           f'dt max: {coords_dt[:, :, -1].max()}')
